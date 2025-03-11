14f7bf8ad427c6b4cfcc37e96b6338db72397173
Prabash <prabhashwijerathna2001@gmail.com>
2025-03-12 04:52:41 +0530

    payment  test

--- src/test/java/org/example/seleniumexample/test/pomFiles/CarBookingandPayment.java
+++ src/test/java/org/example/seleniumexample/test/pomFiles/CarBookingandPayment.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -43,7 +45,7 @@ public class CarBookingandPayment {
         StartDate.sendKeys("03/13/2025");
 
         WebElement EndDate= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='endDate']")));
-        EndDate.sendKeys("03/19/2025");
+        EndDate.sendKeys("03/14/2025");
 
 
         WebElement fromPlace= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@id='fromPlace']//option[@value='Galle'][normalize-space()='Galle']")));
@@ -62,5 +64,37 @@ public class CarBookingandPayment {
 
 
     }
+
+    @Test(priority = 3)
+    public void PaymentForm() {
+        // Wait for payment amount field and get its value
+        WebElement paymentAmountElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(" //input[@id='paymentAmount']")));
+        String paymentAmount = paymentAmountElement.getAttribute("value");
+
+        // Wait for total amount field and get its value
+        WebElement totalAmountElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='totalAmount']")));
+        String totalAmount = totalAmountElement.getAttribute("value");
+
+        // Verify that payment amount and total amount are the same
+        Assert.assertEquals(paymentAmount, totalAmount, "Payment amount does not match total amount!");
+
+        // Check if currency is set to LKR
+        WebElement currencyElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@id='currency']")));
+        Select currencyDropdown = new Select(currencyElement);
+        String selectedCurrency = currencyDropdown.getFirstSelectedOption().getText();
+        Assert.assertEquals(selectedCurrency, "LKR", "Currency is not set to LKR!");
+
+        // Verify transaction ID field is displayed
+        WebElement transactionIdElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='transactionId']")));
+        Assert.assertTrue(transactionIdElement.isDisplayed(), "Transaction ID is not displayed!");
+
+        // Click the "Make Payment" button
+        WebElement makePaymentButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Make Payment']")));
+        makePaymentButton.click();
+
+        System.out.println("Payment process completed successfully.");
+    }
+
+
 
 }
